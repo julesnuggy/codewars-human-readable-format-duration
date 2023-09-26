bdffaa89a942736bcd30077e94b0889de8e29e42
Julian Ng <julesnuggy@gmail.com>
2023-09-26 15:36:47 +0100

    Calculate portions of time units

--- solution.js
+++ solution.js
@@ -11,6 +11,25 @@ const formatDuration = (seconds) => {
     return 'now';
   }
 
+  // Stores to total number of whole units for each time-unit
+  // e.g. ['2 years', '10 days', ...]
+  const timePortion = [];
+  let remainingTime = seconds;
+
+  // [['year', 31536000], ['day', 86400], ...]
+  Object.entries(SECONDS_IN_UNIT).forEach(entry => {
+    const [unitMeasure, unitValue] = entry;
+    const numberOfUnits = remainingTime/unitValue;
+
+    if(numberOfUnits > 1) {
+      const wholeNumberOfUnits = Math.floor(numberOfUnits);
+      const unit = wholeNumberOfUnits > 1 ? `${unitMeasure}s` : unitMeasure;
+      timePortion.push(`${wholeNumberOfUnits} ${unit}`);
+      remainingTime -= wholeNumberOfUnits*unitValue;
+    }
+  });
+
+  return timePortion;
 }
 
-console.log(formatDuration(0));
+console.log(formatDuration(63999999));
