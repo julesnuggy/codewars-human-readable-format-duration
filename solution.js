ab591d30da5266238d774c2cc3d4899c4edeea31
Julian Ng <julesnuggy@gmail.com>
2023-09-26 15:38:04 +0100

    Extract to function

--- solution.js
+++ solution.js
@@ -6,14 +6,10 @@ const SECONDS_IN_UNIT = {
   second: 1,
 };
 
-const formatDuration = (seconds) => {
-  if (seconds === 0) {
-    return 'now';
-  }
-
+const calculateTimePortions = (seconds) => {
   // Stores to total number of whole units for each time-unit
   // e.g. ['2 years', '10 days', ...]
-  const timePortion = [];
+  const timePortions = [];
   let remainingTime = seconds;
 
   // [['year', 31536000], ['day', 86400], ...]
@@ -24,12 +20,21 @@ const formatDuration = (seconds) => {
     if(numberOfUnits > 1) {
       const wholeNumberOfUnits = Math.floor(numberOfUnits);
       const unit = wholeNumberOfUnits > 1 ? `${unitMeasure}s` : unitMeasure;
-      timePortion.push(`${wholeNumberOfUnits} ${unit}`);
+      timePortions.push(`${wholeNumberOfUnits} ${unit}`);
       remainingTime -= wholeNumberOfUnits*unitValue;
     }
   });
 
-  return timePortion;
+  return timePortions;
+}
+
+const formatDuration = (seconds) => {
+  if (seconds === 0) {
+    return 'now';
+  }
+
+  const timePortions = calculateTimePortions(seconds);
+  return timePortions;
 }
 
 console.log(formatDuration(63999999));
